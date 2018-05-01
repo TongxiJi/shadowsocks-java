8e303f2d3e453f0fbcc1f3ed4b73e23d33e22e23
TongxiJi <694270875@qq.com>
2018-05-01 14:13:46 +0800

    log

--- src/main/java/cn/wowspeeder/encryption/CryptUtil.java
+++ src/main/java/cn/wowspeeder/encryption/CryptUtil.java
@@ -10,64 +10,54 @@ import io.netty.buffer.ByteBuf;
 
 public class CryptUtil {
 
-	private static Logger logger = LoggerFactory.getLogger(CryptUtil.class);
+    private static Logger logger = LoggerFactory.getLogger(CryptUtil.class);
 
-	public static byte[] encrypt(ICrypt crypt, Object msg) {
-		byte[] data = null;
-		ByteArrayOutputStream _remoteOutStream = null;
-		try {
-			_remoteOutStream = new ByteArrayOutputStream();
-			ByteBuf bytebuff = (ByteBuf) msg;
-			if (!bytebuff.hasArray()) {
-				int len = bytebuff.readableBytes();
-				byte[] arr = new byte[len];
-				bytebuff.getBytes(0, arr);
-				crypt.encrypt(arr, arr.length, _remoteOutStream);
-				data = _remoteOutStream.toByteArray();
-			} else {
-				crypt.decrypt(bytebuff.array(), bytebuff.readableBytes(), _remoteOutStream);
-				data = _remoteOutStream.toByteArray();
-			}
-		} catch (Exception e) {
-			logger.error("encrypt error", e);
-		} finally {
-			if (_remoteOutStream != null) {
-				try {
-					_remoteOutStream.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-		return data;
-	}
+    public static byte[] encrypt(ICrypt crypt, Object msg) {
+        byte[] data = null;
+        ByteArrayOutputStream _remoteOutStream = null;
+        try {
+            _remoteOutStream = new ByteArrayOutputStream();
+            ByteBuf bytebuff = (ByteBuf) msg;
+            int len = bytebuff.readableBytes();
+            byte[] arr = new byte[len];
+            bytebuff.getBytes(0, arr);
+            crypt.encrypt(arr, arr.length, _remoteOutStream);
+            data = _remoteOutStream.toByteArray();
+        } catch (Exception e) {
+            logger.error("encrypt error", e);
+        } finally {
+            if (_remoteOutStream != null) {
+                try {
+                    _remoteOutStream.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+        return data;
+    }
 
-	public static byte[] decrypt(ICrypt crypt, Object msg) {
-		byte[] data = null;
-		ByteArrayOutputStream _localOutStream = null;
-		try {
-			_localOutStream = new ByteArrayOutputStream();
-			ByteBuf bytebuff = (ByteBuf) msg;
-			if (!bytebuff.hasArray()) {
-				int len = bytebuff.readableBytes();
-				byte[] arr = new byte[len];
-				bytebuff.getBytes(0, arr);
-				crypt.decrypt(arr, arr.length, _localOutStream);
-				data = _localOutStream.toByteArray();
-			} else {
-				crypt.decrypt(bytebuff.array(), bytebuff.readableBytes(), _localOutStream);
-				data = _localOutStream.toByteArray();
-			}
-		} catch (Exception e) {
-			logger.error("encrypt error", e);
-		} finally {
-			if (_localOutStream != null) {
-				try {
-					_localOutStream.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-		return data;
-	}
+    public static byte[] decrypt(ICrypt crypt, Object msg) {
+        byte[] data = null;
+        ByteArrayOutputStream _localOutStream = null;
+        try {
+            _localOutStream = new ByteArrayOutputStream();
+            ByteBuf bytebuff = (ByteBuf) msg;
+            int len = bytebuff.readableBytes();
+            byte[] arr = new byte[len];
+            bytebuff.getBytes(0, arr);
+            crypt.decrypt(arr, arr.length, _localOutStream);
+            data = _localOutStream.toByteArray();
+        } catch (Exception e) {
+            logger.error("encrypt error", e);
+        } finally {
+            if (_localOutStream != null) {
+                try {
+                    _localOutStream.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+        return data;
+    }
 
 }
