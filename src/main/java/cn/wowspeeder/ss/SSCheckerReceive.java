3fa8317c043cce0f1848b0794d8ca0ee0bf47df0
TongxiJi <694270875@qq.com>
2018-07-20 04:34:31 +0800

    check legal datagrampacket

--- src/main/java/cn/wowspeeder/ss/SSCheckerReceive.java
+++ src/main/java/cn/wowspeeder/ss/SSCheckerReceive.java
@@ -29,10 +29,17 @@ public class SSCheckerReceive extends SimpleChannelInboundHandler<Object> {
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
 //        logger.debug("channelRead0");
 
-        ICrypt _crypt = ctx.channel().attr(SSCommon.CIPHER).get();
-
         boolean isUdp = msg instanceof DatagramPacket;
+        DatagramPacket udpRaw = null;
+        if (isUdp) {
+            udpRaw = ((DatagramPacket) msg);
+            if (udpRaw.content().readableBytes() < 4) { //no cipher, min size = 1 + 1 + 2 ,[1-byte type][variable-length host][2-byte port]
+                return;
+            }
+        }
+
 //        logger.debug("channelRead0 isUdp:"+isUdp);
+        ICrypt _crypt = ctx.channel().attr(SSCommon.CIPHER).get();
         if (_crypt == null || isUdp) {
             _crypt = CryptFactory.get(this.method, this.password);
             ctx.channel().attr(SSCommon.CIPHER).set(_crypt);
@@ -40,8 +47,8 @@ public class SSCheckerReceive extends SimpleChannelInboundHandler<Object> {
         ctx.channel().attr(SSCommon.IS_UDP).set(isUdp);
 
         if (isUdp) {
-            ctx.channel().attr(SSCommon.CLIENT).set(((DatagramPacket) msg).sender());
-            ctx.fireChannelRead(((DatagramPacket) msg).content());
+            ctx.channel().attr(SSCommon.CLIENT).set(udpRaw.sender());
+            ctx.fireChannelRead(udpRaw.content());
         } else {
             ctx.channel().attr(SSCommon.IS_FIRST_TCP_PACK).set(true);
             ctx.channel().pipeline().remove(this);
