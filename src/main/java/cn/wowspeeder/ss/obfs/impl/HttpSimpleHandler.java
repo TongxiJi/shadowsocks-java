eb0faa27eee1b27200c2c546ef2c711fa8e16f89
jitongxi <jitongxi0224@163.com>
2019-05-20 10:47:50 +0800

    httpsimple about httpcontent

--- src/main/java/cn/wowspeeder/ss/obfs/impl/HttpSimpleHandler.java
+++ src/main/java/cn/wowspeeder/ss/obfs/impl/HttpSimpleHandler.java
@@ -17,8 +17,8 @@ import java.util.List;
 /**
  * {@link HttpServerCodec} smell nice!
  */
-public class HttpSimpleHandler extends SimpleChannelInboundHandler<HttpRequest> {
-    public static ByteBuf   HTTP_SIMPLE_DELIMITER = Unpooled.copiedBuffer("\r\n".getBytes());
+public class HttpSimpleHandler extends SimpleChannelInboundHandler<HttpObject> {
+    public static ByteBuf HTTP_SIMPLE_DELIMITER = Unpooled.copiedBuffer("\r\n".getBytes());
 
     private static Logger logger = LoggerFactory.getLogger(HttpSimpleHandler.class);
 
@@ -34,29 +34,32 @@ public class HttpSimpleHandler extends SimpleChannelInboundHandler<HttpRequest> 
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, HttpRequest msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
         if (msg.decoderResult() != DecoderResult.SUCCESS) {
             logger.error("simple_http decode error ,pip close");
             ctx.channel().close();
             return;
         }
-        logger.debug(msg.uri());
-        String[] hexItems = msg.uri().split("%");
-        StringBuilder hexStr = new StringBuilder();
-        if (hexItems.length > 1) {
-            for (int i = 1; i < hexItems.length; i++) {
-                if (hexItems[i].length() == 2) {
-                    hexStr.append(hexItems[i]);
-                } else {
-                    hexStr.append(hexItems[i], 0, 2);
-                    break;
+        if (msg instanceof HttpRequest) {
+            logger.debug(((HttpRequest) msg).uri());
+            String[] hexItems = ((HttpRequest) msg).uri().split("%");
+            StringBuilder hexStr = new StringBuilder();
+            if (hexItems.length > 1) {
+                for (int i = 1; i < hexItems.length; i++) {
+                    if (hexItems[i].length() == 2) {
+                        hexStr.append(hexItems[i]);
+                    } else {
+                        hexStr.append(hexItems[i], 0, 2);
+                        break;
+                    }
                 }
             }
+            ByteBuf encodeData = Unpooled.wrappedBuffer(Hex.decode(hexStr.toString()));
+            ctx.writeAndFlush(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK));
+            ctx.fireChannelRead(encodeData);
+        } else if (msg instanceof HttpContent) {
+            ctx.pipeline().remove(HttpServerCodec.class);
+            ctx.pipeline().remove(this);
         }
-        ByteBuf encodeData = Unpooled.wrappedBuffer(Hex.decode(hexStr.toString()));
-        ctx.writeAndFlush(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK));
-        ctx.fireChannelRead(encodeData);
-        ctx.pipeline().remove(HttpServerCodec.class);
-        ctx.pipeline().remove(this);
     }
 }
